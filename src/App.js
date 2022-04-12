7c2fa09b05ef6b0846fc84ae882c4193d9f47711
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-04-12 23:17:11 +0600

    Login Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth,signInWithEmailAndPassword } from 'firebase/auth';
 import app from './firebase_init';
 import { useState } from "react";
 
@@ -12,17 +12,40 @@ const auth=getAuth(app);
 function App() {
   const [email,setEmail]=useState('');
   const [password,setPassword]=useState('');
+  const [error,setError]=useState('');
+  const [registered,setRegistered]=useState(false);
+
+  const handleRegisteredChange=(e)=>{
+    setRegistered(e.target.checked);
+  }
+
   const handleSubmit=(e)=>{
     e.preventDefault();
     console.log("Submitted",email,password);
-    createUserWithEmailAndPassword(auth,email,password)
-    .then((res)=>{
-      const user=res.user;
-      console.log(user);
-    })
-    .catch((error)=>{
-      console.error(error);
-    })
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.error(error);
+          setError(error.message);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          setEmail("");
+          setPassword("");
+        })
+        .catch((error) => {
+          console.error(error);
+          setError(error.message);
+        });
+    }
+    e.preventDefault();
   }
   const emailOnBlur=(e)=>{
     setEmail(e.target.value);
@@ -31,23 +54,42 @@ function App() {
     setPassword(e.target.value);
   }
   return (
-    <div >
+    <div>
       <div className="registration w-50 mx-auto mt-5">
-        <h2 className="text-primary"> Please Register</h2>
+        <h2 className="text-primary"> Please {registered? "Login":"Register"}</h2>
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={emailOnBlur} type="email" placeholder="Enter email" />
+            <Form.Control
+              onBlur={emailOnBlur}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={passwordOnBlur} type="password" placeholder="Password" />
+            <Form.Control
+              onBlur={passwordOnBlur}
+              type="password"
+              placeholder="Password"
+              required
+            />
           </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              onChange={handleRegisteredChange}
+              type="checkbox"
+              label="Already Registered?"
+            />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
+          {console.log(registered)}
           <Button variant="primary" type="submit">
-            Submit
+            {registered? "Login":"Register"}
           </Button>
         </Form>
       </div>
