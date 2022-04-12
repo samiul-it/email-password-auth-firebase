6dd877742145a9bd0be461585bb8687498eaad4b
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-04-12 16:12:58 +0600

    UI Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 function App() {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("Submitted");
+  }
+  const emailOnBlur=(e)=>{
+    console.log(e.target.value);
+  }
+  const passwordOnBlur=(e)=>{
+    console.log(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="registration w-50 mx-auto mt-5">
+        <h2 className="text-primary"> Please Register</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={emailOnBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={passwordOnBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
